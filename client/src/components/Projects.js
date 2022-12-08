7f77334ce5c8e780eb948b2f4f60c1d5bd7858bf
Phantom <montalvod2020@gmail.com>
2022-12-08 02:37:16 -0500

    updated images on bottom

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,9 +1,9 @@
 
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/fau-lg.jpeg";
-import projImg2 from "../assets/img/world.jpeg";
-import projImg3 from "../assets/img/desk.jpeg";
+import projImg1 from "../assets/img/1.jpg";
+import projImg2 from "../assets/img/2.jpg";
+import projImg3 from "../assets/img/3.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -96,6 +96,14 @@ export const Projects = () => {
             </Row>
           </Container>
           <img className="background-image-right" src={colorSharp2}></img>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
         </section>
       )
     }
