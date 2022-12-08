4d663f3d0b0a5799233da5bd08613160be96b505
Phantom <montalvod2020@gmail.com>
2022-12-08 10:16:06 -0500

    updates the CT scans tab
    added stages1-4 and later going to add a bit of a description about every stage of cancer

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -45,7 +45,7 @@ export const NavBar = () => {
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#impact" className={activeLink === 'impact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('impact')}>Impact</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>CT scans</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -12,39 +12,39 @@ export const Projects = () => {
 
     const projects = [
       {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CT SCAN",
+        description: "Stage 1 lung cancer",
         imgUrl: projImg1,
       },
       {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CT SCAN",
+        description: "Stage 1 lung cancer",
         imgUrl: projImg2,
       },
       {  
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CT SCAN",
+        description: "Stage 1 lung cancer",
         imgUrl: projImg3,
       },
       {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CT SCAN",
+        description: "Stage 1 lung cancer",
         imgUrl: projImg1,
       },
       {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CT SCAN",
+        description: "Stage 1 lung cancer",
         imgUrl: projImg2,
       },
       {
-        title: "Business Startup",
-        description: "Design & Development",
+        title: "CT SCAN",
+        description: "Stage 1 lung cancer",
         imgUrl: projImg3,
       },
     ];
 
     return (
-        <section className="project" id="project">
+        <section className="projects" id="projects">
           <Container>
             <Row>
               <Col>
@@ -56,15 +56,17 @@ export const Projects = () => {
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
                       <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                         <Nav.Item>
-                          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                          <Nav.Link eventKey="first">Stage 1</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                          <Nav.Link eventKey="second">Stage 2</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                          <Nav.Link eventKey="third">
-                            Tab 3
-                            </Nav.Link>
+                          <Nav.Link eventKey="third">Stage 3</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="fourth">Stage 4
+                          </Nav.Link>
                         </Nav.Item>
                       </Nav>
                       <Tab.Content>
