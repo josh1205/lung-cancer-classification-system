16f057d1de1eb78d295167c63090a748a7b72de5
Phantom <montalvod2020@gmail.com>
2023-01-20 15:57:10 -0500

    updated client to master
    added the client from phase1 over to the master

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Impact } from "./components/Impact";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Upload } from 'react-bootstrap-icons';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Upload />
+      <Impact />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+
+// class FileUploadButton extends Component {
+//   handleFileUpload = event => {
+//     console.log(event.target.files[0].name);
+//   };
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <input
+//           ref="fileInput"
+//           onChange={this.handleFileUpload}
+//           type="file"
+//           style={{ display: "none" }}
+//           // multiple={false}
+//         />
+//         <button onClick={() => this.refs.fileInput.click()}>Upload File</button>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<FileUploadButton />, rootElement);
