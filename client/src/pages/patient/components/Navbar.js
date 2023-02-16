c9d3cca196cd651a304ed6a05170af63a20efdf3
josh1205 <joshlavieri@gmail.com>
2023-02-16 02:00:10 -0500

    Added bootstrap template for consistincy, fixed incorrect navbar color bug, added more organization to files

--- client/src/pages/patient/components/Navbar.js
+++ client/src/pages/patient/components/Navbar.js
@@ -6,16 +6,29 @@ export default function Navbar() {
     const navigate = useNavigate();
 
     return (
-        <nav>
-            <div className="heading">
-                <h4>Navigation Bar</h4>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <span className="navbar-brand mb-0 h1">Logo</span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                  <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a className="nav-link" href="" onClick={() => navigate('/patient')}>Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a className="nav-link" href="" onClick={() => navigate('/contact-doctor')}>Contact Doctor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a className="nav-link" href="" onClick={() => navigate('/update-userinfo')}>Update Information</a>
+                    </li>
+                    <li class="nav-item">
+                        <a className="nav-link" href="" onClick={() => navigate('/dashboard')}>Logout</a>
+                    </li>
+                  </ul>
+                </div>
             </div>
-            <ul className="topnav">
-                <a className="active" href="" onClick={() => navigate('/patient')}>Home</a>
-                <a href="" onClick={() => navigate('/contact-doctor')}>Contact Doctor</a>
-                <a href="" onClick={() => navigate('/update-userinfo')}>Update Information</a>
-                <a href="" onClick={() => navigate('/dashboard')}>Logout</a>
-            </ul>
         </nav>
     )
 
